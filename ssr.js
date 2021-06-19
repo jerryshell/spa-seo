e418129aca8402738ba4e3c4469eb1561eeaca18
Jerry <calljerryli@outlook.com>
2021-06-19 16:22:19 +0800

    feat: 优化启动参数

--- ssr.js
+++ ssr.js
@@ -3,7 +3,19 @@ const puppeteer = require('puppeteer')
 const ssr = async (url) => {
   console.log('url', url)
 
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process',
+      '--disable-gpu'
+    ],
+    headless: true,
+  })
   const page = await browser.newPage()
 
   try {
